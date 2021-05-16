6ae24d222d206ae8e650be43a4e92aca30c4dcd6
nexus4812 <powerbatteryboy@gmail.com>
2021-05-16 13:58:18 +0900

    init

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,40 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async function () {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const postData = await getPostData(context.params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
