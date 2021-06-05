a1c0d2d4d7e04a11975adfc9e2e3ab032ffb04c3
nexus4812 <powerbatteryboy@gmail.com>
2021-06-05 16:54:41 +0900

    Refact change to typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,39 +1,40 @@
 import fs from 'fs'
 import path from 'path'
-import matter from 'gray-matter'
-import remark from 'remark'
-import html from 'remark-html'
+import matter, {GrayMatterFile, Input} from 'gray-matter'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
+type postData = {
+  id: string
+  date: Date
+  title: string
+}
+
 export function getSortedPostsData() {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
+  const fileNames: string[] = fs.readdirSync(postsDirectory)
+
+  const allPostsData: postData[] = fileNames.map<postData>((fileName: string) => {
     // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '')
+    const id: string = fileName.replace(/\.md$/, '')
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const fullPath: string = path.join(postsDirectory, fileName)
+    const fileContents: string = fs.readFileSync(fullPath, 'utf8')
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents)
+    const matterResult: GrayMatterFile<Input> = matter(fileContents)
 
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      title: matterResult.data.title,
+      date: matterResult.data.date
     }
   })
+
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  })
+  return allPostsData.sort((a:postData, b:postData) => (a.date < b.date ? 1 : -1))
 }
 
 export function getAllPostIds() {
@@ -47,7 +48,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
